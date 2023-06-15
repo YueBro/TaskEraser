5a85e4ae063919c931c453937118ed9091c8df03
YueBro <98621037+YueBro@users.noreply.github.com>
2023-06-15 09:54:33 +0800

    [1.1.0] Update (#1)
    * Add auto save thread
    
    * Completed Up Dn buttons

--- process_hook/misc.py
+++ process_hook/misc.py
@@ -1,9 +1,14 @@
 import json
+from time import sleep
 from logic.logic_func import TaskDb
 from logic.bind_func import BindAttrs
+import threading as thr
 
 
 g_localStoragePth = "tasks.db"
+g_autoSavePeriod = 20
+g_autoSaveThr: thr.Thread = None
+g_autoSaveThrEvnt: thr.Event = thr.Event()
 
 
 def LoadAndAssign(path):
@@ -23,6 +28,7 @@ def LoadAndAssign(path):
     except KeyError:
         print("Warning: local file load fail", "(KeyError)")
 
+
 def Dump(path):
     toDump = {
         "taskDb": TaskDb.taskDb,
@@ -31,3 +37,29 @@ def Dump(path):
     }
     with open(path, "w") as f:
         json.dump(toDump, f)
+
+
+def StartPeriodicAutoSave():
+    global g_autoSaveThr
+    g_autoSaveThr = thr.Thread(
+        target=PeriodicAutoSave,
+        args=(g_autoSaveThrEvnt, g_autoSavePeriod,),
+    )
+    g_autoSaveThr.start()
+
+
+def PeriodicAutoSave(evnt, period):
+    count = 0
+    while not evnt.is_set():
+        if count == 0:
+            print("PeriodicAutoSave", "(auto save!)")
+            Dump(g_localStoragePth)
+        count = (count + 1) % period
+        sleep(1)
+    print("AutoSave terminated")
+
+
+def StopPeriodicAutoSave():
+    if g_autoSaveThr is not None:
+        g_autoSaveThrEvnt.set()
+        g_autoSaveThr.join()
