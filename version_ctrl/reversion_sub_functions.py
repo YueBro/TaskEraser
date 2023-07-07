cc98a0f978ae5e2b914efa80f52fd07cf54700d5
YueBro <98621037+YueBro@users.noreply.github.com>
2023-07-07 15:32:30 +0800

    [v1.2.1] Minor change (#4)
    * Add version_ctrl and TaskRecovery
    
    * remove debugging print
    
    * Defactor storing mechanism of deleted tasks
    
    * refactoring
    
    * Add bin
    
    * 'binCheckBut' bind from Button-1 to ButtonRelease
    
    * Add gitignore

--- version_ctrl/reversion_sub_functions.py
+++ version_ctrl/reversion_sub_functions.py
@@ -1,3 +1,6 @@
+from .misc import g_ver
+
+
 def UpgradeTo_1_2_0(localInfoDict: dict):
     print("Upgrade local task db -> 1.2.0")
     localInfoDict["ver"] = (1, 2, 0)
@@ -5,8 +8,13 @@ def UpgradeTo_1_2_0(localInfoDict: dict):
     localInfoDict["taskOrderDeleted"] = []
     return localInfoDict
 
+# Warning! This function can be used by Admin only!
+def SimpleUpgradeToLatest(localInfoDict: dict, latestVer = g_ver):
+    localInfoDict["ver"] = latestVer
+    return localInfoDict
 
 # ((target_version, function))
 g_UpgradeFuns = (
     ((1,2,0),       UpgradeTo_1_2_0),
+    ((1,2,1),       SimpleUpgradeToLatest),
 )
--- version_ctrl/taskdb_reversion.py
+++ version_ctrl/taskdb_reversion.py
@@ -6,7 +6,10 @@ from .reversion_sub_functions import g_UpgradeFuns
 def ReVersionLocalInfo(localInfoDict: dict):
     for targetVer, func in g_UpgradeFuns:
         if GetVersion(localInfoDict) < targetVer:
+            fromVer = tuple(localInfoDict['ver'])
             localInfoDict = func(localInfoDict)
+            toVer = tuple(localInfoDict['ver'])
+            print(f"Upgrade local task db {fromVer} -> {toVer}")
 
     assert GetVersion(localInfoDict) == g_ver, "Version error, check your code..."
     return localInfoDict
