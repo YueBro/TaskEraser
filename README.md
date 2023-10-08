fe832bd55a6bbacad92cdad49c5f6327738e0969
YueBro <nassonarter@gmail.com>
2023-10-08 23:38:22 +0800

    [v1.2.1] Implement action_notifier

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TaskEraser
+
 Small tool, easily manage your tasks, erase them and gain sense of accomplishment!
+
+
+# Before Merging To `main`
+
+1. Increment `g_ver` in `version_ctrl\misc.py`
+2. Update `g_upgradeFuns` in `version_ctrl\reversion_sub_functions.py`, which allows lower version database be adapted.
+3. Make sure latest commit msg follows `[vX.X.X] Summary of the merge`
--- version_ctrl/reversion_sub_functions.py
+++ version_ctrl/reversion_sub_functions.py
@@ -1,12 +1,21 @@
+from .misc import g_ver
+
+
 def UpgradeTo_1_2_0(localInfoDict: dict):
-    print("Upgrade local task db -> 1.2.0")
     localInfoDict["ver"] = (1, 2, 0)
     localInfoDict["taskDbDeleted"] = dict()
     localInfoDict["taskOrderDeleted"] = []
     return localInfoDict
 
 
+# Warning! This function can be used by Admin only!
+def SimpleUpgradeToLatest(localInfoDict: dict, latestVer = g_ver):
+    localInfoDict["ver"] = latestVer
+    return localInfoDict
+
+
 # ((target_version, function))
-g_UpgradeFuns = (
+g_upgradeFuns = (
     ((1,2,0),       UpgradeTo_1_2_0),
+    ((1,2,1),       SimpleUpgradeToLatest),
 )
